445a5205c3a35c3d62650eb05cbb53fc879254d8
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-28 21:14:37 +0900

    update hello_world2

--- hello_world2/main.v
+++ hello_world2/main.v
@@ -2,18 +2,24 @@ import cc
 
 struct App {
 	cc.BaseApp
+mut:
+	count int
 }
 
 fn (mut app App) setup(){
 	println("setup")
+	app.count = 1
 }
 
 fn (mut app App) update(){
-	// println("update")
+	app.count += 1
+	if app.count > 100 {
+		app.count = 1
+	}
 }
 
 fn (mut app App) draw(){
-	// println("draw")
+	cc.text("count: ${app.count}", 10, 10)
 }
 
 fn (mut app App) exit(){
@@ -22,7 +28,7 @@ fn (mut app App) exit(){
 
 fn main() {
 	cc.run_app_new[App]()
-	
+
 	// mut app := &App{}
 	// cc.run_app(mut app)
 }
