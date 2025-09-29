f0e9ad0fa7ca52c7a76d5f20a2a21f11dcb16715
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-29 17:40:19 +0900

    feat: circle #9

---
+++ tests/circle/main.v
@@ -0,0 +1,19 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import cc
+import gg
+
+fn main() {
+	cc.run(draw)
+}
+
+fn draw(_ voidptr){
+	cc.set_color(gg.red)
+	cc.circle(100, 100, 100)
+
+	cc.no_fill()
+	cc.set_color(gg.blue)
+	cc.circle(120, 120, 100)
+}
